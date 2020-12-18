5eea0f6ded19ab3763cd289f2da33a3d5e2e740a
Cristian <cristianmihai193@gmail.com>
2020-12-18 19:23:04 +0100

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var app = new Vue(
   {
     el: "#root",
     data: {
+      search: "",
       movies: [],
     },
     methods: {
@@ -12,11 +13,25 @@ var app = new Vue(
         .get("https://api.themoviedb.org/3/search/movie?api_key=e08df24142fc22f5d0f9fe1a9fbef609&query=old&language=it-IT")
         .then(function(response){
           self.movies = response.data.results;
+
+          self.movies.forEach(
+            (element) => {
+              console.log(element.title);
+
+              if (element.title.toLowerCase().includes(self.search)) {
+                	element.video = true;
+                } else {
+                  element.video = false;
+                }
+              }
+            );
+
+
+
         })
-        console.log(self.movies);
       }
 
     },
-  
+
   }
 );
