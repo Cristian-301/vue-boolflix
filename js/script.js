724c3db23be8016160506b254828e72a2d473178
Cristian <cristianmihai193@gmail.com>
2020-12-18 18:34:52 +0100

    Show movies onClick

--- js/script.js
+++ js/script.js
@@ -5,17 +5,18 @@ var app = new Vue(
       movies: [],
     },
     methods: {
+      searchMovie: function(){
+        const self = this;
+
+        axios
+        .get("https://api.themoviedb.org/3/search/movie?api_key=e08df24142fc22f5d0f9fe1a9fbef609&query=old&language=it-IT")
+        .then(function(response){
+          self.movies = response.data.results;
+        })
+        console.log(self.movies);
+      }
 
     },
-    mounted: function() {
-      const self = this;
-
-      axios
-      .get("https://api.themoviedb.org/3/search/movie?api_key=e08df24142fc22f5d0f9fe1a9fbef609&query=old&language=it-IT")
-      .then(function(response){
-        self.movies = response.data.results;
-      })
-      console.log(self.movies);
-    }
+  
   }
 );
