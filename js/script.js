7738a7a52b1196f6ffed99f66ce88d783cde194c
Cristian <cristianmihai193@gmail.com>
2020-12-21 17:21:49 +0100

    Aggiunta serie

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var app = new Vue(
     data: {
       search: "",
       movies: [],
+      series: []
     },
     methods: {
       searchMovie: function(){
@@ -13,11 +14,10 @@ var app = new Vue(
         .get("https://api.themoviedb.org/3/search/movie?api_key=e08df24142fc22f5d0f9fe1a9fbef609&query=mission&language=it-IT")
         .then(function(response){
           self.movies = response.data.results;
-          // console.log(self.movies);
+          console.log(response.data.results);
           self.movies.forEach(
             (element) => {
               // console.log(element.title);
-
               if (element.title.toLowerCase().includes(self.search)) {
                 	element.video = true;
                 } else {
@@ -27,10 +27,19 @@ var app = new Vue(
               }
             );
             self.search = "";
+        })
+      },
 
+      searchSeries: function() {
+        const self = this;
+        axios
+        .get("https://api.themoviedb.org/3/tv/on_the_air?api_key=e08df24142fc22f5d0f9fe1a9fbef609")
+        .then(function(response){
+          self.series = response.data.results;
+          console.log(self.series);
         })
 
-      },
+      }
 
 
     },
