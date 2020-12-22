bcf11dcfc174f887df9570cbb73ae700515ccbdd
Cristian <cristianmihai193@gmail.com>
2020-12-22 19:08:43 +0100

    Milestone 2 completata correnttamente

--- js/script.js
+++ js/script.js
@@ -4,45 +4,45 @@ var app = new Vue(
     data: {
       search: "",
       movies: [],
-      series: []
+      series: [],
+
     },
     methods: {
-      searchMovie: function(){
-        const self = this;
-
+      // funzione per ricerca film
+      searchMovies: function() {
+        var self = this;
         axios
-        .get("https://api.themoviedb.org/3/search/movie?api_key=e08df24142fc22f5d0f9fe1a9fbef609&query=mission&language=it-IT")
-        .then(function(response){
-          self.movies = response.data.results;
-          console.log(response.data.results);
-          self.movies.forEach(
-            (element) => {
-              // console.log(element.title);
-              if (element.title.toLowerCase().includes(self.search)) {
-                	element.video = true;
-                } else {
-                  element.video = false;
-                }
-
-              }
-            );
-            self.search = "";
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "e08df24142fc22f5d0f9fe1a9fbef609",
+            query: self.search,
+            language: "it-IT"
+          }
         })
+        .then(function (response) {
+          self.movies = response.data.results
+          console.log("Movies: ", self.movies);
+        })
+
       },
-
-      searchSeries: function() {
-        const self = this;
+      // funzione per ricerca serie
+      searchSeries: function () {
+        var self = this;
         axios
-        .get("https://api.themoviedb.org/3/tv/on_the_air?api_key=e08df24142fc22f5d0f9fe1a9fbef609")
-        .then(function(response){
-          self.series = response.data.results;
-          console.log(self.series);
+        .get("https://api.themoviedb.org/3/search/tv", {
+          params: {
+            api_key: "e99307154c6dfb0b4750f6603256716d",
+            query: self.search,
+            language: "it-IT"
+          }
         })
-
+        .then(function (response) {
+          self.series = response.data.results
+          console.log("Serie: ", self.series);
+        })
+        self.search = "";
       }
 
-
-    },
-
+    }
   }
-);
+)
